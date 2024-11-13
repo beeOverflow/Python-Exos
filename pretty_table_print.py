56b47fa2df59010072d1af93f4f0f9b4ec361c1b
beeOverflow <riyopon@proton.me>
2024-11-13 11:09:09 +0100

    Update pretty_table_print.py

--- pretty_table_print.py
+++ pretty_table_print.py
@@ -1,18 +1,26 @@
 def print_table(table):
-    longest_list_len = max(map(len,[list for list in table])) #get the length of the longest list in the table
+    longest_list_len = max(map(len,[list for list in table]))
+    line = ''
     for i in range(longest_list_len):
         line = ''
         valid_elements = []
         for list in table :
             longest_element = max(map(len,list))
             try:
-                valid_elements.append(list[i]) # get elements to print in a line
+                valid_elements.append(list[i].rjust(longest_element))
             except IndexError :
                 continue
         for element in valid_elements:
-            line += element.rjust(longest_element+5) #rjust to justify 
+            line += element + '\t' 
         print(line)
             
+
+tableData = [['apple', 'oranges', 'cherries', 'banana','pinapple','strawberry'],
+['Alice', 'Bob', 'Carol'],
+['dogs', 'cats', 'moose', 'goose','lion'],
+['Ayanokoji']]
+
+print_table(tableData)
 
 tableData = [['apple', 'oranges', 'cherries', 'banana','pinapple','strawberry'],
 ['Alice', 'Bob', 'Carol'],
